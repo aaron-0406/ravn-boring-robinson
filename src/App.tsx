e7507b677d9fb8f0d1673a516087fd35348b2dd3
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-02-09 20:38:53 -0500

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,7 @@
-import ThemeContextProvider, { useThemeContext } from "./Context/ThemeToggle";
-import UserContextProvider, { useUserContext } from "./Context/UserInfo";
 import "./styles.css";
 
 const DemoOne = () => {
-  return (
-    <div>
-      <ThemeContextProvider>
-        <ThemeSwitcher />
-        <UserContextProvider>
-          <MainLayout />
-        </UserContextProvider>
-      </ThemeContextProvider>
-    </div>
-  );
-};
-
-const ThemeSwitcher = () => {
-  const { toggleTheme } = useThemeContext();
-  console.log("xd");
-
-  return <button onClick={toggleTheme}>Change theme</button>;
-};
-
-const MainLayout = () => {
-  const { isLightMode, light, dark } = useThemeContext();
-  const { name, addCar, updateCar, removeCar } = useUserContext();
-
-  const theme = isLightMode ? light : dark;
-
-  return (
-    <div className="main" style={{ background: theme.bg }}>
-      <div style={{ color: theme.text }}>{`Hello ${name}`}</div>
-      <div>
-        <button onClick={addCar}>Add Car</button>
-        <button onClick={updateCar}>Update Car</button>
-        <button onClick={removeCar}>Remove Car</button>
-      </div>
-    </div>
-  );
+  return <div>HOLA AARON</div>;
 };
 
 export default DemoOne;
